46b01db24c3c73f1ee50e6f4d770e09d3f729158
rposts <rposts>
2025-11-27 20:03:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Envoy
+[Cloud-native high-performance edge/middle/service proxy](https://www.envoyproxy.io/)
+
+Additional information available [here](https://github.com/envoyproxy/envoy).
+
+>**Note:** Artifacts available in this repository are targeted for `s390x` architecture.
---
+++ src/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e -o pipefail
+read -ra arr <<< "$@"
+version=${arr[1]}
+trap 0 1 2 ERR
+# Ensure sudo is installed along with utilities to build rpm and deb packages
+apt-get update && apt-get install sudo -y
+useradd -ms /bin/bash testuser && usermod -aG sudo testuser && echo "%sudo  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+cd /home/testuser
+su testuser -c "bash /tmp/linux-on-ibm-z-scripts/Envoy/${version}/build_envoy.sh -y"
+tar cvfz envoy-${version}-linux-s390x.tar.gz -C $PWD/envoy/bazel-bin/source/exe envoy-static
+exit 0
